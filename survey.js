d700789b8fc2805ed4dff9256bf46bb572a1cf65
Rahul <rahul.shial@gmail.com>
2020-11-03 22:07:02 +0000

    profile generator

---
+++ survey.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
+
+  rl.question('What\'s an activity you like doing? ', (activity) => {
+
+    rl.question('What do you listen to while doing that? ', (listenTo) => {
+
+      rl.question('which meal is your favourite? (eg: dinner, brunch, etc.) ', (favMeal) => {
+
+        rl.question('What\'s your favourite thing to eat for that meal? ', (favMealItem) => {
+
+          rl.question('Which sport is your absolute favourite? ', (sport) => {
+
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
+              
+              console.log(`${name} listens to ${listenTo} while playing ${activity}, loves to eat ${favMealItem} for ${favMeal}, prefers ${sport}, and ${superpower}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
